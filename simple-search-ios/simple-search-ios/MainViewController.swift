d3e910ffef69d82a0ff406a78256584fe7ffdf1e
Kelly Morales <kmorales529@gmail.com>
2022-01-12 13:42:32 -0500

    Adds xcode project with main viewController hookup

---
+++ simple-search-ios/simple-search-ios/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  simple-search-ios
+//
+//  Created by Kelly Morales on 1/12/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ simple-search-ios/simple-search-iosTests/simple_search_iosTests.swift
@@ -0,0 +1,33 @@
+//
+//  simple_search_iosTests.swift
+//  simple-search-iosTests
+//
+//  Created by Kelly Morales on 1/12/22.
+//
+
+import XCTest
+@testable import simple_search_ios
+
+class simple_search_iosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
