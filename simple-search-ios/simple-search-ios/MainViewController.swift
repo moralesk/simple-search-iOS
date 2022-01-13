1a9b3b299ac970e2291b2fdd6d9dc8e5651155f9
Kelly Morales <kmorales529@gmail.com>
2022-01-13 17:45:49 -0500

    Adds starting point for MainViewController UI, and conforms to UISearchResultsUpdating protocol to catch search input

--- simple-search-ios/simple-search-ios/MainViewController.swift
+++ simple-search-ios/simple-search-ios/MainViewController.swift
@@ -9,8 +9,26 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    private let searchController = UISearchController()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Home" // TODO: get from localized strings
+
+        // using default adaptive colors to support light and dark mode
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.tintColor = .systemGray
+
+        searchController.searchBar.placeholder = "Browse artists"
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
     }
 }
 
+extension MainViewController: UISearchResultsUpdating {
+
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchTerm = searchController.searchBar.text
+        print(searchTerm)
+    }
+}
