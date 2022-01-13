539d232d0e14d367a43a66fb04cd4e6979a0f10d
Kelly Morales <kmorales529@gmail.com>
2022-01-13 17:43:45 -0500

    re-adds default view controller & uses scene delegate instead to show MainViewController - this makes the navigation bar work without relying on storyboard settings

--- simple-search-ios/simple-search-ios/AppDelegate.swift
+++ simple-search-ios/simple-search-ios/AppDelegate.swift
@@ -11,11 +11,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let mainViewController = MainViewController()
-
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = mainViewController
-        window.makeKeyAndVisible()
+        // Override point for customization after application launch.
         return true
     }
 
---
+++ simple-search-ios/simple-search-ios/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  simple-search-ios
+//
+//  Created by Kelly Morales on 1/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
