811901e40f64b9ebb8daa77f2a1ea458fedf8e1d
Kelly Morales <kmorales529@gmail.com>
2022-01-14 17:18:02 -0500

    Adds initial tableView UI, adds list of artists to display based on backend response

--- simple-search-ios/simple-search-ios/SearchViewController.swift
+++ simple-search-ios/simple-search-ios/SearchViewController.swift
@@ -10,7 +10,21 @@ import UIKit
 /// ViewController for a simple interface allowing users to search for and select artists
 class SearchViewController: UIViewController {
 
-    private let searchController = UISearchController()
+    private let searchController: UISearchController = {
+        let controller = UISearchController()
+        controller.searchBar.placeholder = "Browse artists"
+        return controller
+    }()
+
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .systemGray6
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    /// The list of artists shown in the tableView
+    private var artists: [Artist]?
 
     /// Class containing logic for search APIs
     var searchNetworkingProvider: SearchNetworkingProviding?
@@ -23,9 +37,10 @@ class SearchViewController: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.tintColor = .systemGray
 
-        searchController.searchBar.placeholder = "Browse artists"
         searchController.searchResultsUpdater = self
         navigationItem.searchController = searchController
+
+        setUpTableView()
     }
 }
 
@@ -39,22 +54,91 @@ extension SearchViewController: UISearchResultsUpdating {
         }
 
         if searchTerm.isEmpty {
-            print("empty")
+            artists?.removeAll()
+            tableView.reloadData()
         } else {
-            searchNetworkingProvider?.fetchArtist(searchTerm) { result in
-                switch result {
-                case .success(let artists):
-                    print(artists)
-                    if artists.isEmpty {
-                        // show empty state
-                    } else {
-                        // populate table with artists
+            DispatchQueue.global().async { [weak self] in
+                self?.searchNetworkingProvider?.fetchArtist(searchTerm) { [weak self] result in
+                    switch result {
+                    case .success(let artists):
+                        if artists.isEmpty {
+                            // show empty state
+                            print("empty")
+                        } else {
+                            self?.artists = artists
+                        }
+                    case .failure(let error):
+                        // show error state
+                        print(error)
                     }
-                case .failure(let error):
-                    // show error state
-                    print(error)
+
+                    DispatchQueue.main.async {
+                        self?.tableView.reloadData()
+                    }
                 }
             }
         }
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension SearchViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // no rows if there are no locally saved artists & nothing in the search bar, or either value is nil
+        if (searchController.searchBar.text?.isEmpty ?? true) && (artists?.isEmpty ?? true) {
+            return 0
+        }
+
+        // at least one row per artist or for an error state
+        return artists?.count ?? 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "test")
+        cell.textLabel?.text = artists?[indexPath.row].name
+        return cell
+    }
+}
+
+// MARK: - Private
+private extension SearchViewController {
+
+    func setUpTableView() {
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        let topConstraint = NSLayoutConstraint(item: tableView,
+                                               attribute: .top,
+                                               relatedBy: .equal,
+                                               toItem: view,
+                                               attribute: .topMargin,
+                                               multiplier: 1,
+                                               constant: 0)
+
+        let leadingConstraint = NSLayoutConstraint(item: tableView,
+                                               attribute: .leading,
+                                               relatedBy: .equal,
+                                               toItem: view,
+                                               attribute: .leading,
+                                               multiplier: 1,
+                                               constant: 0)
+
+        let trailingConstraint = NSLayoutConstraint(item: tableView,
+                                                    attribute: .trailing,
+                                                    relatedBy: .equal,
+                                                    toItem: view,
+                                                    attribute: .trailing,
+                                                    multiplier: 1,
+                                                    constant: 0)
+
+        let bottomConstraint = NSLayoutConstraint(item: tableView,
+                                                  attribute: .bottom,
+                                                  relatedBy: .equal,
+                                                  toItem: view,
+                                                  attribute: .bottom,
+                                                  multiplier: 1,
+                                                  constant: 0)
+
+        NSLayoutConstraint.activate([topConstraint, leadingConstraint, trailingConstraint, bottomConstraint])
     }
 }
