a6bbe478775ff749ef806e17977098e417e44b46
Kelly Morales <kmorales529@gmail.com>
2022-01-14 15:43:45 -0500

    Sets viewcontroller's networking provider when initializingz

--- simple-search-ios/simple-search-ios/SceneDelegate.swift
+++ simple-search-ios/simple-search-ios/SceneDelegate.swift
@@ -18,6 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let searchViewController = SearchViewController()
+        searchViewController.searchNetworkingProvider = SearchNetworkingProvider()
         let navController = UINavigationController(rootViewController: searchViewController)
 
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
