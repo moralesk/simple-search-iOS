824676fb3fea7e4c2cdc25819970d4637aabfd96
Kelly Morales <kmorales529@gmail.com>
2022-01-15 18:34:50 -0500

    Adds TracklistViewController, shows song titles for album

---
+++ simple-search-ios/simple-search-ios/TracklistViewController.swift
@@ -0,0 +1,91 @@
+//
+//  TracklistViewController.swift
+//  simple-search-ios
+//
+//  Created by Kelly Morales on 1/15/22.
+//
+
+import UIKit
+
+/**
+ * ViewController for displaying an album's list of songs
+ */
+class TracklistViewController: UIViewController {
+
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .systemGray6
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    /// The list of tracks displayed in the tableView
+    var tracks: [Track]?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpTableView()
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension TracklistViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracks?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "test")
+        cell.textLabel?.text = tracks?[indexPath.row].title
+        return cell
+    }
+}
+
+// MARK: - Private
+private extension TracklistViewController {
+
+    func setUpTableView() {
+        view.addSubview(tableView)
+        tableView.dataSource = self
+
+        let topConstraint = NSLayoutConstraint(item: tableView,
+                                               attribute: .top,
+                                               relatedBy: .equal,
+                                               toItem: view,
+                                               attribute: .top,
+                                               multiplier: 1,
+                                               constant: 0)
+
+        let leadingConstraint = NSLayoutConstraint(item: tableView,
+                                                   attribute: .leading,
+                                                   relatedBy: .equal,
+                                                   toItem: view,
+                                                   attribute: .leading,
+                                                   multiplier: 1,
+                                                   constant: 0)
+
+        let trailingConstraint = NSLayoutConstraint(item: tableView,
+                                                    attribute: .trailing,
+                                                    relatedBy: .equal,
+                                                    toItem: view,
+                                                    attribute: .trailing,
+                                                    multiplier: 1,
+                                                    constant: 0)
+
+        let bottomConstraint = NSLayoutConstraint(item: tableView,
+                                                  attribute: .bottom,
+                                                  relatedBy: .equal,
+                                                  toItem: view,
+                                                  attribute: .bottom,
+                                                  multiplier: 1,
+                                                  constant: 0)
+
+        NSLayoutConstraint.activate([
+            topConstraint,
+            leadingConstraint,
+            trailingConstraint,
+            bottomConstraint
+        ])
+    }
+}
