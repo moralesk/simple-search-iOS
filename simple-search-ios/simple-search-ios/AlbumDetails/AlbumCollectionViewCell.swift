486e794d9863d1354fd4c281fc269f1fc1e4bc83
Kelly Morales <kmorales529@gmail.com>
2022-01-16 20:56:27 -0500

    uses medium album cover

--- simple-search-ios/simple-search-ios/AlbumDetails/AlbumCollectionViewCell.swift
+++ simple-search-ios/simple-search-ios/AlbumDetails/AlbumCollectionViewCell.swift
@@ -9,14 +9,14 @@ import UIKit
 
 /// Displays an album cover, title, and artist name
 class AlbumCollectionViewCell: UICollectionViewCell {
-
+    
     private var albumImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
-
+    
     private var albumTitleLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: Constants.albumTitleFontSize,
@@ -25,7 +25,7 @@ class AlbumCollectionViewCell: UICollectionViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
+    
     private var artistNameLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: Constants.artistNameFontSize,
@@ -34,20 +34,20 @@ class AlbumCollectionViewCell: UICollectionViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
+    
     /// Contains logic for efficiently fetching UIImages to display in the cell
     var imageNetworkingProvider: ImageNetworkingProviding?
-
+    
     override func prepareForReuse() {
         super.prepareForReuse()
         albumImageView.image = nil
     }
-
+    
     /// Sets up view with the given album and artist name
     func bind(album: Album, artistName: String) {
         setUpView()
-
-        if let imageURL = URL(string: album.cover) {
+        
+        if let imageURL = URL(string: album.coverMedium) {
             setImage(imageURL: imageURL)
         }
         albumTitleLabel.text = album.title
@@ -57,7 +57,7 @@ class AlbumCollectionViewCell: UICollectionViewCell {
 
 // MARK: - Private
 private extension AlbumCollectionViewCell {
-
+    
     struct Constants {
         static let imageViewTopPadding = 8.0
         static let imageViewLeadingTrailingPadding = 0.0
@@ -68,18 +68,18 @@ private extension AlbumCollectionViewCell {
         static let artistNameTopPadding = 4.0
         static let artistNameBottomPadding = 12.0
     }
-
+    
     func setUpView() {
         setUpImageView()
         setUpAlbumTitleLabel()
         setUpArtistNameLabel()
     }
-
+    
     func setUpImageView() {
         if albumImageView.superview == nil {
             contentView.addSubview(albumImageView)
         }
-
+        
         let topConstraint = NSLayoutConstraint(item: albumImageView,
                                                attribute: .top,
                                                relatedBy: .equal,
@@ -87,7 +87,7 @@ private extension AlbumCollectionViewCell {
                                                attribute: .top,
                                                multiplier: 1,
                                                constant: Constants.imageViewTopPadding)
-
+        
         let leadingConstraint = NSLayoutConstraint(item: albumImageView,
                                                    attribute: .leading,
                                                    relatedBy: .equal,
@@ -95,15 +95,15 @@ private extension AlbumCollectionViewCell {
                                                    attribute: .leading,
                                                    multiplier: 1,
                                                    constant: Constants.imageViewLeadingTrailingPadding)
-
+        
         let trailingConstraint = NSLayoutConstraint(item: albumImageView,
-                                                 attribute: .trailing,
-                                                 relatedBy: .equal,
-                                                 toItem: contentView,
-                                                 attribute: .trailing,
-                                                 multiplier: 1,
-                                                 constant: -Constants.imageViewLeadingTrailingPadding)
-
+                                                    attribute: .trailing,
+                                                    relatedBy: .equal,
+                                                    toItem: contentView,
+                                                    attribute: .trailing,
+                                                    multiplier: 1,
+                                                    constant: -Constants.imageViewLeadingTrailingPadding)
+        
         let heightConstraints = NSLayoutConstraint(item: albumImageView,
                                                    attribute: .height,
                                                    relatedBy: .equal,
@@ -111,7 +111,7 @@ private extension AlbumCollectionViewCell {
                                                    attribute: .width,
                                                    multiplier: 1,
                                                    constant: 0)
-
+        
         NSLayoutConstraint.activate([
             topConstraint,
             leadingConstraint,
@@ -119,20 +119,20 @@ private extension AlbumCollectionViewCell {
             heightConstraints
         ])
     }
-
+    
     func setUpAlbumTitleLabel() {
         if albumTitleLabel.superview == nil {
             contentView.addSubview(albumTitleLabel)
         }
-
+        
         let topConstraint = NSLayoutConstraint(item: albumTitleLabel,
-                                                   attribute: .top,
-                                                   relatedBy: .equal,
-                                                   toItem: albumImageView,
-                                                   attribute: .bottom,
-                                                   multiplier: 1,
+                                               attribute: .top,
+                                               relatedBy: .equal,
+                                               toItem: albumImageView,
+                                               attribute: .bottom,
+                                               multiplier: 1,
                                                constant: Constants.albumTitleTopPadding)
-
+        
         let leadingConstraint = NSLayoutConstraint(item: albumTitleLabel,
                                                    attribute: .leading,
                                                    relatedBy: .equal,
@@ -140,7 +140,7 @@ private extension AlbumCollectionViewCell {
                                                    attribute: .leading,
                                                    multiplier: 1,
                                                    constant: Constants.albumTitleLeftRightPadding)
-
+        
         let trailingConstraint = NSLayoutConstraint(item: albumTitleLabel,
                                                     attribute: .trailing,
                                                     relatedBy: .equal,
@@ -148,27 +148,27 @@ private extension AlbumCollectionViewCell {
                                                     attribute: .trailing,
                                                     multiplier: 1,
                                                     constant: -Constants.albumTitleLeftRightPadding)
-
+        
         NSLayoutConstraint.activate([
             topConstraint,
             leadingConstraint,
             trailingConstraint
         ])
     }
-
+    
     func setUpArtistNameLabel() {
         if artistNameLabel.superview == nil {
             contentView.addSubview(artistNameLabel)
         }
-
+        
         let topConstraint = NSLayoutConstraint(item: artistNameLabel,
-                                                   attribute: .top,
-                                                   relatedBy: .equal,
-                                                   toItem: albumTitleLabel,
-                                                   attribute: .bottom,
-                                                   multiplier: 1,
+                                               attribute: .top,
+                                               relatedBy: .equal,
+                                               toItem: albumTitleLabel,
+                                               attribute: .bottom,
+                                               multiplier: 1,
                                                constant: Constants.artistNameTopPadding)
-
+        
         let leadingConstraint = NSLayoutConstraint(item: artistNameLabel,
                                                    attribute: .leading,
                                                    relatedBy: .equal,
@@ -176,7 +176,7 @@ private extension AlbumCollectionViewCell {
                                                    attribute: .leading,
                                                    multiplier: 1,
                                                    constant: Constants.albumTitleLeftRightPadding)
-
+        
         let trailingConstraint = NSLayoutConstraint(item: artistNameLabel,
                                                     attribute: .trailing,
                                                     relatedBy: .equal,
@@ -184,15 +184,15 @@ private extension AlbumCollectionViewCell {
                                                     attribute: .trailing,
                                                     multiplier: 1,
                                                     constant: -Constants.albumTitleLeftRightPadding)
-
+        
         let bottomConstraint = NSLayoutConstraint(item: artistNameLabel,
-                                                    attribute: .bottom,
-                                                    relatedBy: .lessThanOrEqual,
-                                                    toItem: contentView,
-                                                    attribute: .bottom,
-                                                    multiplier: 1,
-                                                    constant: Constants.artistNameBottomPadding)
-
+                                                  attribute: .bottom,
+                                                  relatedBy: .lessThanOrEqual,
+                                                  toItem: contentView,
+                                                  attribute: .bottom,
+                                                  multiplier: 1,
+                                                  constant: Constants.artistNameBottomPadding)
+        
         NSLayoutConstraint.activate([
             topConstraint,
             leadingConstraint,
@@ -200,7 +200,7 @@ private extension AlbumCollectionViewCell {
             bottomConstraint
         ])
     }
-
+    
     func setImage(imageURL: URL) {
         DispatchQueue.global().async { [weak self] in
             self?.imageNetworkingProvider?.fetchImage(from: imageURL) { result in
