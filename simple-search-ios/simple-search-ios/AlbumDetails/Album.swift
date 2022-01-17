8038cb935e5a0f0870183fed3e8c1599b8c9fac4
Kelly Morales <kmorales529@gmail.com>
2022-01-17 10:12:45 -0500

    Removes unused data properties

--- simple-search-ios/simple-search-ios/AlbumDetails/Album.swift
+++ simple-search-ios/simple-search-ios/AlbumDetails/Album.swift
@@ -18,23 +18,11 @@ struct Albums: Decodable {
 struct Album: Decodable {
     let id: Int
     let title: String
-    let link: String
-    let cover: String
-    let coverSmall: String
     let coverMedium: String
-    let releaseDate: String
-    let type: String
-    let isExplicit: Bool
 
     enum CodingKeys: String, CodingKey {
         case id
         case title
-        case link
-        case cover
-        case coverSmall = "cover_small"
         case coverMedium = "cover_medium"
-        case releaseDate = "release_date"
-        case type = "record_type"
-        case isExplicit = "explicit_lyrics"
     }
 }
